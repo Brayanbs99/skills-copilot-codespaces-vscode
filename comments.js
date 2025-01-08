261b47d4142fdad20dcb075dc86397426d624809
Brayanbs99 <64218411+Brayanbs99@users.noreply.github.com>
2025-01-08 16:39:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,48 +1,8 @@
-// Create Web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-// Get all comments
-app.get('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading comments.json');
-            return;
-        }
-
-        res.send(data);
-    });
-});
-
-// Post a comment
-app.post('/comments', (req, res) => {
-    const newComment = req.body;
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading comments.json');
-            return;
-        }
-
-        const comments = JSON.parse(data);
-        comments.push(newComment);
-
-        fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
-            if (err) {
-                res.status(500).send('Error writing comments.json');
-                return;
-            }
-
-            res.send('Comment added');
-        });
-    });
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+// Create Web server with Express
+// Create a route to get comments from the database
+// Create a route to post a comment to the database
+// Create a route to delete a comment from the database
+// Create a route to update a comment in the database
+// Create a route to delete all comments from the database
+// Create a route to update all comments in the database
+// Export the router
