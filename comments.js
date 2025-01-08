1e03e92b378f1ef8422a13d352284a37419dc78a
Brayanbs99 <64218411+Brayanbs99@users.noreply.github.com>
2025-01-08 16:45:14 +0000

    Restart step 4

--- comments.js
+++
@@ -1,8 +0,0 @@
-// Create Web server with Express
-// Create a route to get comments from the database
-// Create a route to post a comment to the database
-// Create a route to delete a comment from the database
-// Create a route to update a comment in the database
-// Create a route to delete all comments from the database
-// Create a route to update all comments in the database
-// Export the router
